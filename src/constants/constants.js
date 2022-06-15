3b611b37ba912599d35f812b96848236bfee7c5b
sp13011996 <githubprojects0508@gmail.com>
2022-06-15 21:17:40 +0530

    images changes

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'Portfolio',
     description: "Created portfolio app using React and Next js",
-    image: '/images/3.jpg',
+    image: './images/3.jpg',
     tags: ['React', 'Next', 'Web Components'],
     source: 'https://github.com/SP13011996/portfolio',
     visit: 'https://github.com/SP13011996/portfolio',
@@ -11,7 +11,7 @@ export const projects = [
   {
     title: 'JS-Mario-Game',
     description: "JS Mario Game using Phaser.js",
-    image: '/images/1.png',
+    image: './images/1.png',
     tags: ['JS', 'HTML', 'Phaser'],
     source: 'https://github.com/SP13011996/JS-Mario-Game',
     visit: 'https://github.com/SP13011996/JS-Mario-Game',
@@ -20,7 +20,7 @@ export const projects = [
   {
     title: 'Flutter App with Rest API',
     description: "Simple Flutter app to get data from news api",
-    image: '/images/2.png',
+    image: './images/2.png',
     tags: ['Flutter', 'REST API'],
     source: 'https://github.com/SP13011996/flutter_rest_api_simple',
     visit: 'https://github.com/SP13011996/flutter_rest_api_simple',
